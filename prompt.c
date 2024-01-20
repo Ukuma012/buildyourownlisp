bdf40383c288bf068876a7b56508f4a3be43aeb7
Ukuma012 <Ukuma012@gmail.com>
2024-01-20 15:42:04 +0900

    editline

--- prompt.c
+++ prompt.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Declare a buffer for user input of size 2048 */
-static char input[2048];
+#include <editline/history.h>
+#include <editline/readline.h>
+
 
 int main(int argc, char** argv) {
     puts("Lispy Version 0.0.0.0.1");
     puts("Press Ctrl+c to Exit\n");
 
     while(1) {
-        fputs("lispy> ", stdout);
+        char* input = readline("mylisp> ");
 
-        fgets(input, 2048, stdin);
+        add_history(input);
 
-        printf("No you're a %s", input);
+        printf("%s\n", input);
+
+        free(input);
     }
     return 0;
 }
