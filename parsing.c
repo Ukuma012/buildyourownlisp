67687d852eede355725252da539cd35c5c6839c8
Ukuma012 <Ukuma012@gmail.com>
2024-01-21 10:50:30 +0900

    evaluation

--- parsing.c
+++ parsing.c
@@ -24,6 +24,38 @@ void add_history(char* unused) {}
 #include <editline/readline.h>
 #endif
 
+/* Use operator string to see which operation to perform */
+long eval_op (long x, char* op, long y) {
+    if (strcmp(op, "+") == 0) {return x + y;}
+    if (strcmp(op, "-") == 0) {return x - y;}
+    if (strcmp(op, "*") == 0) {return x * y;}
+    if (strcmp(op, "/") == 0) {return x / y;}
+    return 0;
+}
+
+long eval(mpc_ast_t* t) {
+
+    /* If tagged as number return it directly. */
+    if (strstr(t->tag, "number")) {
+        return atoi(t->contents);
+    }
+
+    /* The operator is alywas second child */
+    char* op = t->children[1]->contents;
+
+    /* We store the third child in 'x' */
+    long x = eval(t->children[2]);
+
+    /* Iterate the remaining children and combining */
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+
+    return x;
+}
+
 
 int main(int argc, char** argv) {
 
@@ -54,15 +86,14 @@ int main(int argc, char** argv) {
 
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Lispy, &r)) {
-            /* On Success Print the AST */
-            mpc_ast_print(r.output);
+
+            long result = eval(r.output);
+            printf("%li\n", result);
             mpc_ast_delete(r.output);
         } else {
             mpc_err_print(r.error);
             mpc_err_delete(r.error);
         }
-
-
 
         free(input);
     }
