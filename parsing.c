6544d06f4ef66bb337fc5eef950ad9735ac8d280
Ukuma012 <Ukuma012@gmail.com>
2024-01-24 21:23:49 +0900

    refactoring using macro

--- parsing.c
+++ parsing.c
@@ -26,6 +26,9 @@ void add_history(char* unused) {}
 #include <editline/history.h>
 #endif
 
+#define LASSERT(args, cond, err) \
+  if (!(cond)) { lval_del(args); return lval_err(err); }
+
 /* Add SYM and SEXPR as possible lval types */
 enum { LVAL_ERR, LVAL_NUM, LVAL_SYM, LVAL_SEXPR, LVAL_QEXPR };
 
@@ -214,21 +217,14 @@ lval* builtin_op(lval* a, char* op) {
 }
 
 lval* buildin_head(lval* a) {
-  /* Check Error Conditions */
-  if (a->count != 1) {
-    lval_del(a);
-    return laval_error("Function 'head' passed too many arguments!");
-  }
+  LASSERT(a, a->count == 1,
+  "Function 'head' passed too many arguments!");
 
-  if (a->cell[0]->type != LVAL_QEXPR) {
-    lval_del(a);
-    return lval_err("Function 'head' passed incorrect types!");
-  }
+  LASSERT(a, a->cell[0]->type == LVAL_QEXPR,
+  "Function 'head' passed incorrect type!");
 
-  if (a->cell[0]->count == 0) {
-    lval_del(a);
-    return lval_err("Function 'head' passed ()!");
-  }
+  LASSERT(a, a->cell[0]->count != 0,
+  "Function 'head' passed ()!");
 
   /* Otherwise take first argument */
   lval* v = lval_take(a, 0);
@@ -241,21 +237,14 @@ lval* buildin_head(lval* a) {
 }
 
 lval* builtin_tail(lval* a) {
-  /* Check Error Conditions */
-  if (a->count != 1) {
-    lval_del(a);
-    return lval_err("Function 'tail' passed too many arguments");
-  }
+  LASSERT(a, a->count != 1,
+  "Function 'tail' passed too many arguments")
 
-  if (a->cell[0]->type != LVAL_QEXPR) {
-    lval_del(a);
-    return lval_err("Function 'tail' passed incorrect types");
-  }
+  LASSERT(a, a->cell[0]->type == LVAL_QEXPR,
+  "Function 'tail' passed incorrect types")
 
-  if (a->cell[0]->count == 0) {
-    lval_del(a);
-    return lval_err("Function 'tail' passed ()!");
-  }
+  LASSERT(a, a->cell[0]->count != 0,
+  "Function 'tail' passed ()!");
 
   /* Take first argument */
   lval* v = lval_take(a, 0);
